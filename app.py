cd91c2e1b59155613d3c7e76645fd7654c617c15
EXPERIMIND LABS <142927350+experimindlabs@users.noreply.github.com>
2025-01-31 16:18:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,6 @@ def update_inventory(project_name):
     mongo.db.projects.update_one({"name": project_name}, {"$set": {"inventory": inventory}})
     return jsonify({"message": "Inventory updated"}), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
